e6dbd10ae9eaae5f9585eb91cc3bd0204ed43247
xware-net <xware.net@yandex.com>
2024-09-14 19:27:36 +0200

    merge gtnflafgs into names.cs

--- ManagedPlugin64/IdaNet/IdaInterop/Name.cs
+++ ManagedPlugin64/IdaNet/IdaInterop/Name.cs
@@ -19,6 +19,20 @@ using System.Runtime.InteropServices;
 
 namespace IdaNet.IdaInterop
 {
+    public enum GTNFlags : int
+    {
+        GN_VISIBLE = 0x0001, ///< replace forbidden characters by SUBSTCHAR
+        GN_COLORED = 0x0002, ///< return colored name
+        GN_DEMANGLED = 0x0004, ///< return demangled name
+        GN_STRICT = 0x0008, ///< fail if cannot demangle
+        GN_SHORT = 0x0010, ///< use short form of demangled name
+        GN_LONG = 0x0020, ///< use long form of demangled name
+        GN_LOCAL = 0x0040, ///< try to get local name first; if failed, get global
+        GN_ISRET = 0x0080, ///< for dummy names: use retloc
+        GN_NOT_ISRET = 0x0100, ///< for dummy names: do not use retloc
+        GN_NOT_DUMMY = 0x0200, ///< do not return a dummy name
+    }
+
     public enum demreq_type_t
     {
         DQT_NPURGED_8 = -8, // only calculate number of purged bytes (sizeof(arg)==8)
